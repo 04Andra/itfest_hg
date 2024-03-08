e511f41d2a6ec10da6a513aa4e5e5d3ec6366383
04Andra <andra.malaescu04@e-uvt.ro>
2024-03-08 21:06:12 +0200

    kspcer

--- screens/auth/Landing.js
+++ screens/auth/Landing.js
@@ -1,10 +1,51 @@
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet, ImageBackground} from "react-native";
 
 function Landing() {
+
+    const backgroundImage = require('C:\\Users\\Acer\\Desktop\\ITFest\\itfest_hg\\assets\\photos\\wallpaperGreen.png');
+
     return(
-        <View>
-            <Text>Login page</Text>
+        <View style={landingStyles.container1}>
+
+            <ImageBackground
+                style={landingStyles.backgroundImage}
+                source={backgroundImage}
+                resizeMode="cover"
+            >
+
+                <View>
+
+                </View>
+
+                <View>
+
+                </View>
+
+                <View>
+
+                </View>
+
+                <View>
+
+                </View>
+
+            </ImageBackground>
+
         </View>
     )
 }
+
+const landingStyles = StyleSheet.create({
+    container1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    backgroundImage: {
+        flex: 1,
+        justifyContent: 'center',
+        width: '100%',
+    }
+});
+
 export default Landing;
